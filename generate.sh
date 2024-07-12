f013a29c956f9d57c25551c64dc49eb80bee9f0a
Maysun J Faisal <maysunaneek@gmail.com>
2024-07-12 17:05:01 -0400

    test
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
 # get app templates
-$ROOTDIR/scripts/update-tekton-definition
-$ROOTDIR/scripts/import-gitops-template
-$ROOTDIR/scripts/import-ai-lab-samples
+# $ROOTDIR/scripts/update-tekton-definition
+# $ROOTDIR/scripts/import-gitops-template
+# $ROOTDIR/scripts/import-ai-lab-samples
+touch $ROOTDIR/test.txt
+echo TEST >> $ROOTDIR/test.txt
